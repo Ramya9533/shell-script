b94845fff1c7035eaca81acc5e7dff02a93c5ae0
Ramya9533 <Ramya953375@gmail.com>
2024-10-16 13:27:27 +0530

    install multiple packages file we can observe

--- 13-Install-package.sh
+++ 13-Install-package.sh
@@ -4,7 +4,27 @@
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP"  &>> $LOGFILE
+
+VALIDATE(){
+    IF [ $1 -ne 0 ]
+    then
+      echo -e $2 ... $R FAILED $N"
+
+    else
+      echo -e $2 ... $G SUCCESS $N"
+
+      fi
+
+         
+
+}
 
 ID=$(id-u)
 
@@ -19,3 +39,24 @@ else
 fi # fi means reverse of if, indicating condition end
 
 echo "All arguments passed: $@"
+# git mysql postfix net-tools
+# package=git for first time
+
+for package in $@
+do 
+
+   yum list installed $package &>> $LOGFILE #check installed or not
+
+   if [$? -ne 0 ] #if not installed
+
+   then
+
+      Yum install $package -y &>> $LOGFILE # install the package
+      VALIDATE $? "Installation of $package" # validate
+   else
+      
+      echo -e "$package is already installed... $Y SKIPPING $N"
+
+   fi   
+
+ done  
